8a0c987fdf7319a755653665afab27de7f421014
Ry00mni <e666.metal@gmail.com>
2025-03-16 11:04:58 +0900

    .sqlファイルからSQL文読み込み機能を追加

--- src/app1/UpdateDB.java
+++ src/app1/UpdateDB.java
@@ -1,36 +1,57 @@
 package app1;
 
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class UpdateDB {
 	public static void main(String args[]) {
+		
 		/* DB接続情報 */
-	    final String URL = "jdbc:mysql://localhost/shop";
+	    final String URL = "jdbc:mysql://localhost/Shop";
 		final String USER = "root";
 		final String PASS = "";
-		
-		/* 実行用SQL */
-		final String sql = "START TRANSACTION;\n"
-				+ "\n"
-				+ "INSERT INTO Shohin VALUES ('0001', 'Tシャツ' ,'衣服', 1000, 500, '2009-09-20');\n"
-				+ "INSERT INTO Shohin VALUES ('0002', '穴あけパンチ', '事務用品', 500, 320, '2009-09-11');\n"
-				+ "INSERT INTO Shohin VALUES ('0003', 'カッターシャツ', '衣服', 4000, 2800, NULL);\n"
-				+ "INSERT INTO Shohin VALUES ('0004', '包丁', 'キッチン用品', 3000, 2800, '2009-09-20');\n"
-				+ "INSERT INTO Shohin VALUES ('0005', '圧力鍋', 'キッチン用品', 6800, 5000, '2009-01-15');\n"
-				+ "INSERT INTO Shohin VALUES ('0006', 'フォーク', 'キッチン用品', 500, NULL, '2009-09-20');\n"
-				+ "INSERT INTO Shohin VALUES ('0007', 'おろしがね', 'キッチン用品', 880, 790, '2008-04-28');\n"
-				+ "INSERT INTO Shohin VALUES ('0008', 'ボールペン', '事務用品', 100, NULL, '2009-11-11');\n"
-				+ "\n"
-				+ "COMMIT;";
 		
 		/* Data Access Objectの生成 */
 		DAO dao = new DAO(URL, USER, PASS);
 		
+		/* 
+		 * ターミナルにて入力受付
+		 * 入力された絶対パスの.sqlファイルを読み込み
+		 */
+		String sql = "";
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("実行するSQLファイルの絶対パスを入力してください");
+		System.out.println(">");
+		String filePath = scanner.nextLine();
+		scanner.close();
+		
+		try {
+			// sqlの読み込み
+			sql = new String(Files.readAllBytes(Paths.get(filePath)),StandardCharsets.UTF_8);
+			System.out.println(sql);  // デバッグ用
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		} 
+		
 		/* SQL分の解析、実行*/
 		dao.validationSQL(sql);
-	}
-	
-	
-	
-	
+//			
+//		String filePath = "/Users/ryosuke/Pictures/test.sql";
+//        StringBuilder sql = new StringBuilder();
+//
+//        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+//            String line;
+//            while ((line = br.readLine()) != null) {
+//                sql.append(line).append("\n");
+//            }
+//            System.out.println(sql.toString());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }	
 		
-
+//			dao.validationSQL(sql.toString());
+	}
 }
